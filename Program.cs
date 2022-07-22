9ad4c28056f78fcd92fded8458b8dacf040dd2bc
RiadR555 <63714904+RiadR555@users.noreply.github.com>
2022-07-22 11:47:29 +0200

    SQL_Connector
    Ich habe gestern alles probiert. Es funktioniert einfach nicht auf mein Betriebssystem. Bzw. Ich vermute, dass es daran liegt, dass man TCP/IP aktivieren muss, aber die Einstellung finde ich nicht. Kannst du es bitte bei dir probieren ?

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source = 24e6b0c39456\\MySQL ; Initial Catalog = Chipkarte_system; User ID = root; Password=Riadr555";
+
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                Console.WriteLine("The database has been opened!");
+                Console.WriteLine("Connection State: " + connection.State.ToString());
+
+                connection.Close();
+                Console.WriteLine("The database has been closed!");
+
+                connection.Dispose();
+                Console.WriteLine("The database connection has been disposed!");
+                Console.WriteLine("Connection State: " + connection.State.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There's an error connecting to the database!\n" + ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
