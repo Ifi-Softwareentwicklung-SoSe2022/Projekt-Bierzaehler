a6d8983d9128f4c5979f18c6cbb4b9da2ef194f1
Nagelow <92780933+Nagelow@users.noreply.github.com>
2022-07-22 21:19:32 +0200

    Update Form2.Designer.cs

--- BierzaehlerUI/Form2.Designer.cs
+++ BierzaehlerUI/Form2.Designer.cs
@@ -44,16 +44,7 @@
             this.dataGridView1.Size = new System.Drawing.Size(800, 300);
             this.dataGridView1.TabIndex = 0;
           
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(400, 400);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(200, 100);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Drucken";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+
             // 
             // button2
             // 
